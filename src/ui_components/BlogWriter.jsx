4edc02ca9ef4be7fe10e2631651378601f7f5105
CHAITHRA C <142213358+chaithra-03@users.noreply.github.com>
2025-07-05 21:21:37 +0530

    Update BlogWriter.jsx

--- src/ui_components/BlogWriter.jsx
+++ src/ui_components/BlogWriter.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import pic from "../images/pic.jpg"
 import FormatDate from "../services/FormatDate"
+import BASE_URL from '../api';
 
 
 const BlogWriter = ({blog}) => {
@@ -12,7 +13,7 @@ const BlogWriter = ({blog}) => {
       <span className="flex items-center gap-2">
         <div className="w-[40px] h-[40px] rounded-full overflow-hidden">
           <img
-            src={`http://localhost:8001${blog.author.profilepic}`}
+            src={`${BASE_URL}${blog.author.profilepic}`}
             className="c rounded-full w-full h-full object-cover"
           />
         </div>
